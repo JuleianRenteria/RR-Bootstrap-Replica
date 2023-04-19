ff80faf4ed81990e1403d741a0beaff834be575f
Juleian Renteria <jprenteria02@gmail.com>
2023-04-19 16:26:31 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import topBar from './Components/TopBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      < topBar />
     </div>
   );
 }
